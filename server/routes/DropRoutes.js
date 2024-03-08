db2d1caaaadef8e985ebcc6503d22079b5a39bd9
Veda19g <vedang19gambhire@gmail.com>
2024-03-08 12:16:02 +0530

    routing done

--- server/routes/DropRoutes.js
+++ server/routes/DropRoutes.js
@@ -7,6 +7,10 @@ const authMiddleware = require('../middleware/authMiddleware');
 router.get('/getDrops', authMiddleware,  dropController.getAllDrops);
 router.get('/getDrop', authMiddleware , dropController.getDrop)
 router.post('/addAnonymousDrop', authMiddleware, dropController.addAnonymousDrop)
-router.post('/addDirectDrop', authMiddleware, dropController.addDirectDrop)
+router.post('/addDirectDrop', authMiddleware, dropController.addDirectDrop);
+router.get('/getDropForUser', authMiddleware, dropController.getDropForUser);
+router.post('/likeDrop', authMiddleware, dropController.likeDrop);
+router.post('/removeLikeDrop', authMiddleware, dropController.removelikeDrop);
+
 
 module.exports = router;
--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -6,5 +6,8 @@ const authMiddleware = require('../middleware/authMiddleware');
 router.post("/signup", userController.createNewUser);
 router.post('/login', userController.userLogin)
 router.get("/checkStatus", authMiddleware, userController.checkStatus);
+router.post("/friendRequest", authMiddleware, userController.sendFriendRequest);
+router.post("/acceptRequest", authMiddleware, userController.acceptFriendRequest);
+router.post("/rejectRequest", authMiddleware, userController.rejectFriendRequest);
 
 module.exports = router
